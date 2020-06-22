204849dfd656053a4ad407dc665c52e536e127ee
Silvano Sallese <silvano.sallese@gmail.com>
2020-06-22 22:39:01 +0200

    keywords group

--- tera-mode.el
+++ tera-mode.el
@@ -66,8 +66,8 @@
      ( ,(rx "{{" (* whitespace) (group (*? anything))
             (* "|" (* whitespace) (*? anything)) (* whitespace) "}}") (1 font-lock-variable-name-face t))
      ( ,(rx  (group "|" (* whitespace)) (group (+ word))) (1 font-lock-keyword-face t) (2 font-lock-warning-face t))
-     ;; ( ,(rx-to-string `(and (group "|" (* whitespace)) (group ,(append '(or) (builtin-functions-keywords))))) (1 font-lock-keyword-face t) (2 font-lock-function-name-face t))
-     ;; ( ,(rx-to-string `(and word-start ,(append '(or) (constructs-keywords)) word-end)) (0 font-lock-builtin-face))
+     ( ,(rx-to-string `(and (group "|" (* whitespace)) (group ,(append '(or) builtin-functions-keywords)))) (1 font-lock-keyword-face t) (2 font-lock-function-name-face t))
+     ( ,(rx-to-string `(and word-start ,(append '(or) construct-keywords) word-end)) (0 font-lock-builtin-face))
      )))
 
 ;;;
